816c5e0637c6fdae2d5cfdc52a6a0ca47443fcea
Chase Brown <chaseb686@gmail.com>
2018-11-07 01:28:33 -0700

    Update6

--- main.py
+++ main.py
@@ -43,6 +43,8 @@ print("Using vocabulary size %d." % len(index_to_char))
 print("The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1]))
 print(char_freq.most_common(10))
 f = open('report.doc', 'w')
+# f = open('grad.doc', 'w') # swap file open for grad checking models.
+
 # Create the training data
 XTrain = np.asarray([[char_to_index[w] for w in sent[:-1]] for sent in toke_n])
 yTrain = np.asarray([[char_to_index[w] for w in sent[1:]] for sent in toke_n])
@@ -51,17 +53,34 @@ x_example, y_example = XTrain[42], yTrain[42]
 print("x:\n%s\n%s" % (" ".join([index_to_char[x] for x in x_example]), x_example))
 print("\ny:\n%s\n%s" % (" ".join([index_to_char[x] for x in y_example]), y_example))
 # ++++++++++++++++++++++++Gradient Check++++++++++++++++++++++++++++++++++++++++++++
-# np.random.seed(10)
-# grad_check_vocab_size = 100
-# model = rnn.RNNVanilla(grad_check_vocab_size, 10, bptt_truncate=1000)
-# model.gradient_check([31, 40, 20, 43], [40, 20, 43, 50], f)
-# # ========================rnn vanilla 100 hidden units==============================
-# model = rnn.RNNVanilla(len(index_to_char))
-# # Limit to 1000 examples to save time
-# print("Expected Loss for random predictions: %f" % np.log(len(index_to_char)))
-# print("Actual loss: %f" % model.calculate_loss(XTrain[:1000], yTrain[:1000]))
-# f.write("Expected Loss for random predictions: %f\n" % np.log(len(index_to_char)))
-# f.write("Actual loss: %f\n" % model.calculate_loss(XTrain[:1000], yTrain[:1000]))
+# grad_check_vocab_size = 256
+# model = rnn.RNNVanilla(grad_check_vocab_size, 100, bptt_truncate=4)
+# model.gradient_check(XTrain[42], yTrain[42], f)
+# model = rnn.RNNVanilla(grad_check_vocab_size, 50, bptt_truncate=4)
+# model.gradient_check(XTrain[42], yTrain[42], f)
+# model = rnn.RNNVanilla(grad_check_vocab_size, 200, bptt_truncate=4)
+# model.gradient_check(XTrain[42], yTrain[42], f)
+# toke_n = []
+# for i in range(len(toke_sent)):
+#     if len(toke_sent[i]) == 25:
+#         toke_n.append(toke_sent[i])
+# # Create the training data for the new sequence
+# XTrain = np.asarray([[char_to_index[w] for w in sent[:-1]] for sent in toke_n])
+# yTrain = np.asarray([[char_to_index[w] for w in sent[1:]] for sent in toke_n])
+# model = rnn.RNNVanilla(grad_check_vocab_size, 100, bptt_truncate=4)
+# model.gradient_check(XTrain[42], yTrain[42], f)
+# toke_n = []
+# for i in range(len(toke_sent)):
+#     if len(toke_sent[i]) == 100:
+#         toke_n.append(toke_sent[i])
+# # Create the training data for the new sequence
+# XTrain = np.asarray([[char_to_index[w] for w in sent[:-1]] for sent in toke_n])
+# yTrain = np.asarray([[char_to_index[w] for w in sent[1:]] for sent in toke_n])
+# model = rnn.RNNVanilla(grad_check_vocab_size, 100, bptt_truncate=4)
+# model.gradient_check(XTrain[42], yTrain[42], f)
+# f.close()
+# +++++++++++++++++++++++++++++++++++++++end grad checking++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# =========================rnn vanilla 100 hidden units=============================================================
 
 loss = []
 # Train
@@ -107,42 +126,42 @@ loss.append(L)
 f.close()
 # # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # ************************************LSTM Start***********************************************************************
-# data = ''
-# for file in os.listdir('stories'):
-#     with open('stories/'+file, encoding='utf8') as f:
-#         data = data + f.read()
-#
-# chars = list(set(data))
-# data_size, X_size = len(data), len(chars)
-# f = open('reportlstm.doc', 'w')
-# print("data has %d characters, %d unique" % (data_size, X_size))
-# f.write("data has %d characters, %d unique\n" % (data_size, X_size))
-# char_to_idx = {ch: i for i, ch in enumerate(chars)}
-# idx_to_char = {i: ch for i, ch in enumerate(chars)}
-#
-# print(data_size)
-# print(X_size)
-# print(char_to_idx)
-# print(idx_to_char)
-# # Constants and hyper-params
-# H_size = 100  # Size of the hidden layer
-# T_steps = 25  # Number of time steps (length of the sequence) used for training
-# learning_rate = 1e-1  # Learning rate
-# weight_sd = 0.1  # Standard deviation of weights for initialization
-# z_size = H_size + X_size  # Size of concatenate(H, X) vector
-#
-# lstm.lstm(f, H_size, z_size, weight_sd, X_size, learning_rate, T_steps, data, idx_to_char, char_to_idx, 'lstm100hidden')
-# H_size = 50
-# z_size = H_size + X_size
-# lstm.lstm(f, H_size, z_size, weight_sd, X_size, learning_rate, T_steps, data, idx_to_char, char_to_idx, 'lstm50hidden')
-# H_size = 200
-# z_size = H_size + X_size
-# lstm.lstm(f, H_size, z_size, weight_sd, X_size, learning_rate, T_steps, data, idx_to_char, char_to_idx, 'lstm200hidden')
-# H_size = 100
-# z_size = H_size + X_size
-# T_steps = 15
-# lstm.lstm(f, H_size, z_size, weight_sd, X_size, learning_rate, T_steps, data, idx_to_char, char_to_idx, 'lstmSeq15')
-# T_steps = 50
-# lstm.lstm(f, H_size, z_size, weight_sd, X_size, learning_rate, T_steps, data, idx_to_char, char_to_idx, 'lstmSeq50')
-#
-# f.close()
+data = ''
+for file in os.listdir('stories'):
+    with open('stories/'+file, encoding='utf8') as f:
+        data = data + f.read()
+
+chars = list(set(data))
+data_size, X_size = len(data), len(chars)
+f = open('reportlstm.doc', 'w')
+print("data has %d characters, %d unique" % (data_size, X_size))
+f.write("data has %d characters, %d unique\n" % (data_size, X_size))
+char_to_idx = {ch: i for i, ch in enumerate(chars)}
+idx_to_char = {i: ch for i, ch in enumerate(chars)}
+
+print(data_size)
+print(X_size)
+print(char_to_idx)
+print(idx_to_char)
+# Constants and hyper-params
+H_size = 100  # Size of the hidden layer
+T_steps = 25  # Number of time steps (length of the sequence) used for training
+learning_rate = 1e-1  # Learning rate
+weight_sd = 0.1  # Standard deviation of weights for initialization
+z_size = H_size + X_size  # Size of concatenate(H, X) vector
+
+lstm.lstm(f, H_size, z_size, weight_sd, X_size, learning_rate, T_steps, data, idx_to_char, char_to_idx, 'lstm100hidden')
+H_size = 50
+z_size = H_size + X_size
+lstm.lstm(f, H_size, z_size, weight_sd, X_size, learning_rate, T_steps, data, idx_to_char, char_to_idx, 'lstm50hidden')
+H_size = 200
+z_size = H_size + X_size
+lstm.lstm(f, H_size, z_size, weight_sd, X_size, learning_rate, T_steps, data, idx_to_char, char_to_idx, 'lstm200hidden')
+H_size = 100
+z_size = H_size + X_size
+T_steps = 15
+lstm.lstm(f, H_size, z_size, weight_sd, X_size, learning_rate, T_steps, data, idx_to_char, char_to_idx, 'lstmSeq15')
+T_steps = 50
+lstm.lstm(f, H_size, z_size, weight_sd, X_size, learning_rate, T_steps, data, idx_to_char, char_to_idx, 'lstmSeq50')
+
+f.close()
